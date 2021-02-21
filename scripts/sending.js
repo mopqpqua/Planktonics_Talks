4f22c226db45cf38a2561d6cd60a7e378d5e2778
mopqpqua <m3atyy@gmail.com>
2021-02-21 16:07:30 +0500

    Made a few documentation.

--- scripts/sending.js
+++ scripts/sending.js
@@ -8,8 +8,8 @@ const vm = new Vue({
 
   methods: {
     sentMessage: function() {
-      let msgInput = document.querySelector('.chat__input');
-      let msgTo = document.querySelector('.chat__contacts');
+      let msgInput = document.querySelector('.talking__input');
+      let msgTo = document.querySelector('.talking__contacts');
       this.messages.push(`${msgTo.value}: ${msgInput.value}`);
       msgInput.value = '';
     },
