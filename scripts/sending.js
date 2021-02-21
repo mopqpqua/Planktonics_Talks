19344721d89c6bec9c5b163a7e0e7c6090c7b993
mopqpqua <m3atyy@gmail.com>
2021-02-21 19:02:56 +0500

    Add authentication.

--- scripts/sending.js
+++ scripts/sending.js
@@ -3,15 +3,34 @@
 const vm = new Vue({
   el: '.chat',
   data: {
+    auth: false,
+    // Пользователи
+    user: {},
+    users: {
+      'sp@gmail.com': { login: 'Саша', password: 'poetry' },
+    },
+
+    // Сообщения
+    message: '',
     messages: [],
   },
 
   methods: {
     sentMessage: function() {
-      let msgInput = document.querySelector('.talking__input');
-      let msgTo = document.querySelector('.talking__contacts');
-      this.messages.push(`${msgTo.value}: ${msgInput.value}`);
-      msgInput.value = '';
+      this.messages.push({ name: this.user, text: this.message, date: '' });
+      this.message = '';
+    },
+
+    login: function() {
+      let currentUser = this.user.email;
+      if ( Object.keys(this.users).includes(currentUser) ) {
+        this.users[currentUser].password == this.user.password &&
+        this.users[currentUser].login == this.user.login ?
+        this.auth = true :
+        alert('Неверный логин или пароль');
+      } else {
+        alert('Пользователь не зарегестрирован');
+      };
     },
   },
 });
